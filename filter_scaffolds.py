586e4985c8c147505c70490e1dfb313b36ba60c3
Katherine <katherine.kelly@ucdconnect.ie>
2020-02-25 22:01:26 +0100

    filter high GC

--- filter_scaffolds.py
+++ filter_scaffolds.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-count =0
+import re
 short=0
+GC=0
 with open(sys.argv[1], 'r') as fin, open('filtered.fa', 'w') as fout:
     for line in fin:
         line=line.rstrip()
@@ -11,5 +12,7 @@ with open(sys.argv[1], 'r') as fin, open('filtered.fa', 'w') as fout:
                     short=0
                 else:
                     short=1
-        if not short:
+        else:
+            GC=float(len(re.findall('[GCgc]', line))/len(line))
+        if GC<0.3 and not short:        # filter out reads <3000 bp and those with GC%>30
             print(line, file=fout)
